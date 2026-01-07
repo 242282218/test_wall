06d3acaf9e066f598b6e98ce02de1aaacac61cc1
Xzc_cccc <99485603+242282218@users.noreply.github.com>
2026-01-07 14:09:03 +0800

    Delete tmdb_wall directory

--- tmdb_wall/app/__init__.py
+++
@@ -1,2 +0,0 @@
-# Package marker for TMDB 海报墙应用
-
--- tmdb_wall/app/quark/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-夸克搜索工具模块
-"""
-from app.quark.utils.link_validator import LinkValidator, validate_quark_link
-
-__all__ = ["LinkValidator", "validate_quark_link"]
